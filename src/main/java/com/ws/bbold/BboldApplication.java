3bf0bbbf903fa219af32d89b5fe2557acc36cb55
r0czak <kaczorb.kontakt@gmail.com>
2022-10-09 23:45:39 +0200

    Init project

---
+++ src/main/java/com/ws/bbold/BboldApplication.java
@@ -0,0 +1,13 @@
+package com.ws.bbold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BboldApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BboldApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ws/bbold/BboldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ws.bbold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BboldApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
