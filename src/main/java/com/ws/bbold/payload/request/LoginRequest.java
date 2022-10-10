a205d4d786aa239e3656694a4e58f43bf2dfee1f
r0czak <kaczorb.kontakt@gmail.com>
2022-10-10 03:33:06 +0200

    Add JWT Authentication
     - Add JWT Authentication
     - Add Test Controllers for JWT Authentication
     - Add Liquibase integration

---
+++ src/main/java/com/ws/bbold/payload/request/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.ws.bbold.payload.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Setter
+@Getter
+public class LoginRequest {
+  @NotBlank private String username;
+
+  @NotBlank private String password;
+}
---
+++ src/main/java/com/ws/bbold/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.ws.bbold.security.services;
+
+import com.ws.bbold.models.User;
+import com.ws.bbold.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+  @Autowired UserRepository userRepository;
+
+  @Override
+  @Transactional
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user =
+        userRepository
+            .findByUsername(username)
+            .orElseThrow(
+                () -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+    return UserDetailsImpl.build(user);
+  }
+}
